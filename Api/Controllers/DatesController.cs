63a0c2ad291bf16b089dfbb09bb6a8d143eea679
Ricardo Milan <ricardo@fluencypass.com>
2022-06-17 08:45:31 -0300

    implementando IServiceProvider e FromServices

---
+++ Api/Controllers/DatesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatesController : ControllerBase
+    {
+        [HttpGet("now")]
+        public DateTime GetNow([FromServices] IDateService dateService)
+        {
+            return dateService.Now();
+        }
+    }
+}
---
+++ Services/Implementations/DateService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services.Interfaces;
+
+namespace Services.Implementations
+{
+    internal class DateService : IDateService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DateService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public DateTime Now()
+        {
+            var studentService = _serviceProvider.GetService<IStudentService>();
+
+            ArgumentNullException.ThrowIfNull(studentService);
+
+            var students = studentService.Get();
+
+            return DateTime.Now.AddDays(students.Count());
+        }
+    }
+}
